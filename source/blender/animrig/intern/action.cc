zNightlord/Bforartists

commit 7dbb338c9bb63236101b888aea31d9a1e35b4f07

Fix #156094: Symmetrize armature duplicates FCurves of Action Constraints
When symmetrizing an armature, FCurves of action constraints on said
armature are also symmetrized. It does so by duplicating FCurves from
one side to another. This didn't check if the FCurve on the other side
existed already though. So this could create duplicate FCurves in
the action.

The fix is to check if the FCurve already exists and use it in case
it does. To ensure that the symmetrized size still matches the original,
the bezt array is duplicated.

Pull Request: https://projects.blender.org/blender/blender/pulls/156299

source/blender/animrig/intern/action.cc
@@ -1928,6 +1928,28 @@ Vector<FCurve *> Channelbag::fcurve_create_many(Main *bmain,
   return new_fcurves;
 }
 
+FCurve &Channelbag::fcurve_clone(const FCurve &old_fcurve,
+                                 const StringRefNull new_path,
+                                 const int new_array_index,
+                                 const StringRef new_group_name)
+{
+  FCurve *new_fcurve = this->fcurve_find({new_path, new_array_index});
+  if (new_fcurve) {
+    MEM_delete(new_fcurve->bezt);
+    new_fcurve->bezt = MEM_dupalloc(old_fcurve.bezt);
+  }
+  else {
+    new_fcurve = BKE_fcurve_copy(&old_fcurve);
+    MEM_delete(new_fcurve->rna_path);
+    new_fcurve->rna_path = BLI_strdup(new_path.data());
+    new_fcurve->array_index = new_array_index;
+    this->fcurve_append(*new_fcurve);
+  }
+  bActionGroup &agrp = this->channel_group_ensure(new_group_name.data());
+  this->fcurve_assign_to_channel_group(*new_fcurve, agrp);
+  return *new_fcurve;
+}
+
 void Channelbag::fcurve_append(FCurve &fcurve)
 {
   /* Appended F-Curves don't belong to any group yet, so better make sure their

source/blender/animrig/ANIM_action.hh
@@ -1079,6 +1079,16 @@ class Channelbag : public ActionChannelbag {
   Vector<FCurve *> fcurve_create_many(Main *bmain, Span<FCurveDescriptor> fcurve_descriptors);
 
   /**
+   * Duplicates the FCurve and changes the data of the duplicate to the given `new_...` values.
+   * In case an FCurve with `new_path` and `new_array_index` already exists, the keys in it are
+   * replaced with the keys of old_fcurve and it is moved to `new_group_name`.
+   */
+  FCurve &fcurve_clone(const FCurve &old_fcurve,
+                       StringRefNull new_path,
+                       int new_array_index,
+                       StringRef new_group_name);
+
+  /**
    * Append an F-Curve to this Channelbag.
    *
    * This transfers ownership of the F-Curve to this Channelbag, and it is up to

source/blender/editors/armature/armature_add.cc
@@ -535,45 +535,42 @@ static void update_duplicate_action_constraint_settings(
     animrig::Action &action = act->wrap();
     animrig::Channelbag *cbag = animrig::channelbag_for_action_slot(action,
                                                                     act_con->action_slot_handle);
-
-    /* Create a copy and mirror the animation */
-    auto bone_name_filter = [&](const FCurve &fcurve) -> bool {
-      return animrig::fcurve_matches_collection_path(fcurve, "pose.bones[", orig_bone->name);
-    };
-    Vector<FCurve *> fcurves = animrig::fcurves_in_action_slot_filtered(
-        act, act_con->action_slot_handle, bone_name_filter);
-    for (const FCurve *old_curve : fcurves) {
-      FCurve *new_curve = BKE_fcurve_copy(old_curve);
-      char *old_path = new_curve->rna_path;
-
-      new_curve->rna_path = BLI_string_replaceN(old_path, orig_bone->name, dup_bone->name);
-      MEM_delete(old_path);
-
-      /* FIXME: deal with the case where this F-Curve already exists. */
-
+    Span<FCurve *> fcurves = {};
+    if (cbag) {
+      fcurves = cbag->fcurves();
+    }
+    for (const FCurve *old_fcurve : fcurves) {
+      if (!animrig::fcurve_matches_collection_path(*old_fcurve, "pose.bones[", orig_bone->name)) {
+        continue;
+      }
+      const char *new_path = BLI_string_replaceN(
+          old_fcurve->rna_path, orig_bone->name, dup_bone->name);
+      FCurve &new_curve = cbag->fcurve_clone(
+          *old_fcurve, new_path, old_fcurve->array_index, dup_bone->name);
+      MEM_delete(new_path);
       /* Flip the animation */
       int i;
       BezTriple *bezt;
-      for (i = 0, bezt = new_curve->bezt; i < new_curve->totvert; i++, bezt++) {
-        const size_t slength = strlen(new_curve->rna_path);
+      for (i = 0, bezt = new_curve.bezt; i < new_curve.totvert; i++, bezt++) {
+        const size_t slength = strlen(new_curve.rna_path);
         bool flip = false;
-        if (BLI_strn_endswith(new_curve->rna_path, "location", slength) &&
-            new_curve->array_index == 0)
+        if (BLI_strn_endswith(new_curve.rna_path, "location", slength) &&
+            new_curve.array_index == 0)
         {
           flip = true;
         }
-        else if (BLI_strn_endswith(new_curve->rna_path, "rotation_quaternion", slength) &&
-                 ELEM(new_curve->array_index, 2, 3))
+        else if (BLI_strn_endswith(new_curve.rna_path, "rotation_quaternion", slength) &&
+                 ELEM(new_curve.array_index, 2, 3))
         {
           flip = true;
         }
-        else if (BLI_strn_endswith(new_curve->rna_path, "rotation_euler", slength) &&
-                 ELEM(new_curve->array_index, 1, 2))
+        else if (BLI_strn_endswith(new_curve.rna_path, "rotation_euler", slength) &&
+                 ELEM(new_curve.array_index, 1, 2))
         {
           flip = true;
         }
-        else if (BLI_strn_endswith(new_curve->rna_path, "rotation_axis_angle", slength) &&
-                 ELEM(new_curve->array_index, 2, 3))
+        else if (BLI_strn_endswith(new_curve.rna_path, "rotation_axis_angle", slength) &&
+                 ELEM(new_curve.array_index, 2, 3))
         {
           flip = true;
         }
@@ -584,10 +581,6 @@ static void update_duplicate_action_constraint_settings(
           bezt->vec[2][1] *= -1;
         }
       }
-      BLI_assert_msg(cbag, "If there are F-Curves for this slot, there should be a channelbag");
-      bActionGroup &agrp = cbag->channel_group_ensure(dup_bone->name);
-      cbag->fcurve_append(*new_curve);
-      cbag->fcurve_assign_to_channel_group(*new_curve, agrp);
     }
   }
 

Zezo-Ai/blender

commit 0f71cc919b609b2092294944bbf28e8778bd2c4d

Fix: Potentially incorrect vertex count for FCurves after cloning
Caused by 7dbb338c9bb63236101b888aea31d9a1e35b4f07

In that commit, I forgot to update the totvert of the FCurve in
`fcurve_clone`. This could lead to issues if the vertex count of the
existing fcurve, does not match the count of the curve to clone.
This patch also checks for `fpt` on fcurves and copies those.

Found by brechts LLM

Pull Request: https://projects.blender.org/blender/blender/pulls/157581

source/blender/animrig/intern/action.cc
@@ -1937,6 +1937,9 @@ FCurve &Channelbag::fcurve_clone(const FCurve &old_fcurve,
   if (new_fcurve) {
     MEM_delete(new_fcurve->bezt);
     new_fcurve->bezt = MEM_dupalloc(old_fcurve.bezt);
+    MEM_delete(new_fcurve->fpt);
+    new_fcurve->fpt = MEM_dupalloc(old_fcurve.fpt);
+    new_fcurve->totvert = old_fcurve.totvert;
   }
   else {
     new_fcurve = BKE_fcurve_copy(&old_fcurve);