Zezo-Ai/blender

commit e70ba644f15a4c5f702934615e6496e7698c1892

Nodes: Drag multiple selected sockets into editor
Now that the Group interface supports selecting multiple sockets/panels,
the operator that allows dragging sockets into the editor (creating a
Group Input node with the selected socket/panel) can be improved to
expose multiple selected sockets in the input node.

Like before, any selected panel causes all its items to be exposed in
the new node, unless the panel has a header toggle, in which case, the
toggle is dragged alone. Holding `Ctrl` still allows changing this
behavior, and the tooltip offers this hint when at least one panel
toggle is being dragged. Alternatively, it is also easy to select both
the panel and its children and drag them all to achieve the same result.

Since many items can now be dragged, the tooltip no longer includes the
name of the dragged item.

Pull Request: https://projects.blender.org/blender/blender/pulls/157639

source/blender/editors/interface/templates/interface_template_node_tree_interface.cc
@@ -440,7 +440,6 @@ void *NodeTreeInterfaceDragController::create_drag_data() const
 
   bNodeTreeInterfaceItemReference *drag_data = MEM_new_zeroed<bNodeTreeInterfaceItemReference>(
       __func__);
-  drag_data->item = &item_;
   drag_data->tree = &tree_;
   drag_data->items_count = drag_items.size();
   drag_data->items = MEM_new_array_zeroed<bNodeTreeInterfaceItem *>(drag_data->items_count,

source/blender/blenkernel/BKE_node_tree_interface.hh
@@ -424,7 +424,7 @@ bNodeTreeInterfaceSocket *add_interface_socket_from_node(
     std::optional<eNodeSocketInOut> in_out = std::nullopt);
 
 /**
- * Reference to a node tree's interface item.
+ * Reference to a node tree's dragged interface items.
  *
  * Used by the node interface drag controller to reorder interface items and
  * the node space drop-boxes to drop Group Input/Output nodes into the node
@@ -432,9 +432,6 @@ bNodeTreeInterfaceSocket *add_interface_socket_from_node(
  */
 struct bNodeTreeInterfaceItemReference {
   bNodeTree *tree;
-  /* The item under the cursor when dragging started. Used to create Group Input node in the node
-   * editor. */
-  bNodeTreeInterfaceItem *item;
   /* All dragged items. If a parent item is selected, its children are excluded because they are
    * dragged implicitly. */
   bNodeTreeInterfaceItem **items;

source/blender/editors/space_node/node_add.cc
@@ -1337,91 +1337,56 @@ void NODE_OT_add_import_node(wmOperatorType *ot)
 /** \name Add Group Input Node Operator
  * \{ */
 
+static void hide_unselected_sockets(bNode *node,
+                                    bNodeTreeInterfaceItem *item,
+                                    bool panels_with_header_unselected)
+{
+  switch (eNodeTreeInterfaceItemType(item->item_type)) {
+    case NODE_INTERFACE_SOCKET: {
+      auto *socket = reinterpret_cast<bNodeTreeInterfaceSocket *>(item);
+      if (socket->flag & NODE_INTERFACE_SOCKET_INPUT &&
+          !(socket->flag & NODE_INTERFACE_SOCKET_SELECT))
+      {
+        auto *node_socket = node->output_by_identifier(UString(socket->identifier));
+        node_socket->flag |= SOCK_HIDDEN;
+      }
+      break;
+    }
+    case NODE_INTERFACE_PANEL: {
+      /* Only visit unselected panels. */
+      auto *interface_panel = reinterpret_cast<bNodeTreeInterfacePanel *>(item);
+      bool panel_selection_ignored = panels_with_header_unselected &&
+                                     interface_panel->header_toggle_socket();
+      if (!(interface_panel->flag & NODE_INTERFACE_PANEL_SELECT) || panel_selection_ignored) {
+        for (auto *sub_item : interface_panel->items()) {
+          hide_unselected_sockets(node, sub_item, panels_with_header_unselected);
+        }
+      }
+      break;
+    }
+  }
+}
+
 static wmOperatorStatus node_add_group_input_node_exec(bContext *C, wmOperator *op)
 {
   SpaceNode *snode = CTX_wm_space_node(C);
   bNodeTree *ntree = snode->edittree;
+  bNodeTreeInterface &interface = ntree->tree_interface;
 
-  bool single_socket = false;
-  char socket_identifier[int(sizeof(bNodeSocket::idname))];
-  bool single_panel = false;
-  int panel_identifier = 0;
-  if (RNA_struct_property_is_set(op->ptr, "socket_identifier")) {
-    single_socket = true;
-    RNA_string_get(op->ptr, "socket_identifier", socket_identifier);
-  }
-  if (RNA_struct_property_is_set(op->ptr, "panel_identifier")) {
-    single_panel = true;
-    panel_identifier = RNA_int_get(op->ptr, "panel_identifier");
-  }
-  if (single_socket && single_panel) {
-    BKE_report(op->reports, RPT_ERROR, "Cannot set both socket and panel identifier");
-    return OPERATOR_CANCELLED;
-  }
-
-  bNodeTreeInterfacePanel *interface_panel = nullptr;
-
-  if (single_socket) {
-    /* Ensure the requested socket exists in the node interface. */
-    bNodeTreeInterfaceSocket *interface_socket = nullptr;
-    for (bNodeTreeInterfaceSocket *tsocket : ntree->interface_inputs()) {
-      if (STREQ(socket_identifier, tsocket->identifier)) {
-        interface_socket = tsocket;
-        break;
-      }
-    }
-    if (!interface_socket) {
-      BKE_report(
-          op->reports,
-          RPT_ERROR,
-          fmt::format("Invalid socket_identifier: Socket \"%s\" not found", socket_identifier)
-              .c_str());
-      return OPERATOR_CANCELLED;
-    }
-  }
-  if (single_panel) {
-    /* Ensure the requested panel exists in the node interface. */
-    for (bNodeTreeInterfaceItem *item : ntree->interface_items()) {
-      bNodeTreeInterfacePanel *tpanel = bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(
-          item);
-      if (tpanel && tpanel->identifier == panel_identifier) {
-        interface_panel = tpanel;
-        break;
-      }
-    }
-
-    if (!interface_panel) {
-      BKE_report(op->reports, RPT_ERROR, "Invalid panel identifier");
-      return OPERATOR_CANCELLED;
-    }
-  }
+  const bool only_selected = RNA_boolean_get(op->ptr, "only_selected_sockets");
+  const bool all_panel_contents = RNA_boolean_get(op->ptr, "all_panel_contents");
 
   ED_preview_kill_jobs(CTX_wm_manager(C), CTX_data_main(C));
 
   bNode *group_input_node = add_node(*C, "NodeGroupInput"_ustr, snode->runtime->cursor);
 
-  if (single_socket) {
-    /* Hide all other sockets in the new node, to only display the selected one. */
-    for (bNodeSocket &socket : group_input_node->outputs) {
-      if (!STREQ(socket.identifier, socket_identifier)) {
-        socket.flag |= SOCK_HIDDEN;
-      }
-    }
-  }
-  if (single_panel) {
-    /* Initially hide all sockets. */
-    for (bNodeSocket &socket : group_input_node->outputs) {
-      socket.flag |= SOCK_HIDDEN;
-    }
-    /* Show only sockets contained in the dragged panel. */
-    for (bNodeTreeInterfaceSocket *iface_socket : ntree->interface_inputs()) {
-      if (interface_panel->contains_recursive(iface_socket->item)) {
-        bNodeSocket *socket = bke::node_find_socket(
-            *group_input_node, SOCK_OUT, iface_socket->identifier);
-        BLI_assert(socket);
-        socket->flag &= ~SOCK_HIDDEN;
-      }
-    }
+  if (only_selected) {
+    hide_unselected_sockets(group_input_node,
+                            reinterpret_cast<bNodeTreeInterfaceItem *>(&interface.root_panel),
+                            !all_panel_contents);
+
+    /* Hide __extend__ socket. */
+    group_input_node->output_by_identifier("__extend__"_ustr)->flag |= SOCK_HIDDEN;
   }
 
   return OPERATOR_FINISHED;
@@ -1447,6 +1412,29 @@ static wmOperatorStatus node_add_group_input_node_invoke(bContext *C,
   return node_add_group_input_node_exec(C, op);
 }
 
+static bool contains_any_selected_input(const bNodeTreeInterfaceItem &item, bool parent_selected)
+{
+  switch (eNodeTreeInterfaceItemType(item.item_type)) {
+    case NODE_INTERFACE_SOCKET: {
+      const auto &socket = reinterpret_cast<const bNodeTreeInterfaceSocket &>(item);
+      return socket.flag & NODE_INTERFACE_SOCKET_INPUT &&
+             (parent_selected || socket.flag & NODE_INTERFACE_SOCKET_SELECT);
+    }
+    case NODE_INTERFACE_PANEL: {
+      const auto &panel = reinterpret_cast<const bNodeTreeInterfacePanel &>(item);
+      for (const auto *sub_item : panel.items()) {
+        /* There's no need to handle the header toggle differently. */
+        if (contains_any_selected_input(
+                *sub_item, parent_selected || panel.flag & NODE_INTERFACE_PANEL_SELECT))
+        {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
 static bool node_add_group_input_node_poll(bContext *C)
 {
   if (!ED_operator_node_editable(C)) {
@@ -1455,34 +1443,16 @@ static bool node_add_group_input_node_poll(bContext *C)
 
   const SpaceNode *snode = CTX_wm_space_node(C);
   bNodeTree *ntree = snode->edittree;
+  bNodeTreeInterface &interface = ntree->tree_interface;
 
-  bNodeTreeInterface interface = ntree->tree_interface;
-  bNodeTreeInterfaceItem *active_item = interface.active_item();
-
-  if (auto *socket = bke::node_interface::get_item_as<bNodeTreeInterfaceSocket>(active_item)) {
-    if (socket->flag & NODE_INTERFACE_SOCKET_OUTPUT) {
-      CTX_wm_operator_poll_msg_set(C, "Cannot drag an output socket");
-      return false;
-    }
-    return true;
+  if (!contains_any_selected_input(
+          reinterpret_cast<bNodeTreeInterfaceItem &>(interface.root_panel), false))
+  {
+    CTX_wm_operator_poll_msg_set(C, "No selected input sockets or panels");
+    return false;
   }
 
-  if (auto *panel = bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(active_item)) {
-    bool has_inputs = false;
-    for (bNodeTreeInterfaceSocket *socket : ntree->interface_inputs()) {
-      if (panel->contains_recursive(socket->item)) {
-        has_inputs = true;
-        break;
-      }
-    }
-
-    if (!has_inputs) {
-      CTX_wm_operator_poll_msg_set(C, "Cannot drag panel with no inputs");
-      return false;
-    }
-    return true;
-  }
-  return false;
+  return true;
 }
 
 void NODE_OT_add_group_input_node(wmOperatorType *ot)
@@ -1497,23 +1467,20 @@ void NODE_OT_add_group_input_node(wmOperatorType *ot)
 
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO | OPTYPE_INTERNAL;
 
-  PropertyRNA *prop = RNA_def_string(ot->srna,
-                                     "socket_identifier",
-                                     nullptr,
-                                     int(sizeof(bNodeSocket::idname)),
-                                     "Socket Identifier",
-                                     "Socket to include in the added group input/output node");
-  RNA_def_property_flag(prop, PROP_SKIP_SAVE | PROP_HIDDEN);
-  prop = RNA_def_int(ot->srna,
-                     "panel_identifier",
-                     0,
-                     INT_MIN,
-                     INT_MAX,
-                     "Panel Identifier",
-                     "Panel from which to add sockets to the added group input/output node",
-                     INT_MIN,
-                     INT_MAX);
-  RNA_def_property_flag(prop, PROP_SKIP_SAVE | PROP_HIDDEN);
+  PropertyRNA *prop = RNA_def_boolean(
+      ot->srna,
+      "only_selected_sockets",
+      true,
+      "Only Selected Sockets",
+      "Include only selected sockets/panels in the added group input node");
+  RNA_def_property_flag(prop, PROP_SKIP_SAVE);
+  prop = RNA_def_boolean(
+      ot->srna,
+      "all_panel_contents",
+      false,
+      "All Panel Contents",
+      "Include sockets in all selected panels, even if they have a panel toggle");
+  RNA_def_property_flag(prop, PROP_SKIP_SAVE);
 }
 
 /** \} */

source/blender/editors/space_node/space_node.cc
@@ -1065,7 +1065,9 @@ static bool node_import_file_drop_poll(bContext *C, wmDrag *drag, const wmEvent 
   return false;
 }
 
-static bool node_socket_drop_poll(bContext *C, wmDrag *drag, const wmEvent *event)
+static bool node_interface_item_drop_poll_base(bContext *C,
+                                               wmDrag *drag,
+                                               const wmEvent * /*event*/)
 {
   if (drag->type != WM_DRAG_NODE_TREE_INTERFACE) {
     return false;
@@ -1085,62 +1087,17 @@ static bool node_socket_drop_poll(bContext *C, wmDrag *drag, const wmEvent *even
     return false;
   }
 
-  /* Accept only socket items. */
-  const bNodeTreeInterfaceSocket *socket =
-      bke::node_interface::get_item_as<bNodeTreeInterfaceSocket>(drag_data->item);
-  if (socket) {
-    /* The check to avoid dragging output sockets is deferred to the
-     * operator's poll in order to display a hint tooltip. */
-    return true;
-  }
-
-  /* Unless Ctrl is held, prefer dragging the toggle socket alone from a panel with toggle. */
-  if (!(event->modifier & KM_CTRL)) {
-    const bNodeTreeInterfacePanel *panel =
-        bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(drag_data->item);
-    if (panel && panel->header_toggle_socket()) {
-      return true;
-    }
-  }
-  return false;
+  return true;
 }
 
-static bool node_panel_drop_poll(bContext *C, wmDrag *drag, const wmEvent *event)
+static bool node_interface_item_drop_poll_single(bContext *C, wmDrag *drag, const wmEvent *event)
 {
-  if (drag->type != WM_DRAG_NODE_TREE_INTERFACE) {
-    return false;
-  }
-  SpaceNode *snode = CTX_wm_space_node(C);
-  if (!snode || !snode->edittree) {
-    return false;
-  }
-  const bNodeTree *target_ntree = snode->edittree;
+  return !(event->modifier & KM_CTRL) && node_interface_item_drop_poll_base(C, drag, event);
+}
 
-  auto *drag_data = static_cast<bke::node_interface::bNodeTreeInterfaceItemReference *>(
-      drag->poin);
-
-  /* Drag only onto node editors of the same node. */
-  const bNodeTree *source_ntree = drag_data->tree;
-  if (target_ntree != source_ntree) {
-    return false;
-  }
-
-  /* Accept only panel items. */
-  const bNodeTreeInterfacePanel *panel = bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(
-      drag_data->item);
-  if (panel) {
-    /* Unless Ctrl is held, prefer dragging only the toggle socket of a panel with toggle. */
-    if (!(event->modifier & KM_CTRL)) {
-      if (panel->header_toggle_socket()) {
-        return false;
-      }
-    }
-
-    /* The check for whether the panel contains at least one input socket is
-     * deferred to the operator's poll in order to display a hint tooltip. */
-    return true;
-  }
-  return false;
+static bool node_interface_item_drop_poll_all(bContext *C, wmDrag *drag, const wmEvent *event)
+{
+  return event->modifier & KM_CTRL && node_interface_item_drop_poll_base(C, drag, event);
 }
 
 static void node_group_drop_copy(bContext *C, wmDrag *drag, wmDropBox *drop)
@@ -1176,84 +1133,51 @@ static void node_import_file_drop_copy(bContext * /*C*/, wmDrag *drag, wmDropBox
   io::paths_to_operator_properties(drop->ptr, WM_drag_get_paths(drag));
 }
 
-static void node_socket_drop_copy(bContext * /*C*/, wmDrag *drag, wmDropBox *drop)
+static void node_interface_item_drop_copy_single(bContext * /*C*/,
+                                                 wmDrag * /*drag*/,
+                                                 wmDropBox *drop)
 {
-  if (drag->type != WM_DRAG_NODE_TREE_INTERFACE) {
-    return;
-  }
-
-  auto *drag_data = static_cast<bke::node_interface::bNodeTreeInterfaceItemReference *>(
-      drag->poin);
-  const bNodeTreeInterfaceSocket *socket =
-      bke::node_interface::get_item_as<bNodeTreeInterfaceSocket>(drag_data->item);
-  if (!socket) {
-    const bNodeTreeInterfacePanel *panel =
-        bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(drag_data->item);
-    socket = panel->header_toggle_socket();
-  }
-
-  BLI_assert(socket);
-  PropertyRNA *prop = RNA_struct_find_property(drop->ptr, "panel_identifier");
-  RNA_property_unset(drop->ptr, prop);
-  RNA_string_set(drop->ptr, "socket_identifier", socket->identifier);
+  RNA_boolean_set(drop->ptr, "only_selected_sockets", true);
+  RNA_boolean_set(drop->ptr, "all_panel_contents", false);
 }
 
-static void node_panel_drop_copy(bContext * /*C*/, wmDrag *drag, wmDropBox *drop)
+static void node_interface_item_drop_copy_all(bContext * /*C*/, wmDrag * /*drag*/, wmDropBox *drop)
 {
-  if (drag->type != WM_DRAG_NODE_TREE_INTERFACE) {
-    return;
-  }
-
-  auto *drag_data = static_cast<bke::node_interface::bNodeTreeInterfaceItemReference *>(
-      drag->poin);
-  const bNodeTreeInterfacePanel *panel = bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(
-      drag_data->item);
-
-  BLI_assert(panel);
-  PropertyRNA *prop = RNA_struct_find_property(drop->ptr, "socket_identifier");
-  RNA_property_unset(drop->ptr, prop);
-  RNA_int_set(drop->ptr, "panel_identifier", panel->identifier);
+  RNA_boolean_set(drop->ptr, "only_selected_sockets", true);
+  RNA_boolean_set(drop->ptr, "all_panel_contents", true);
 }
 
-static std::string node_socket_drop_tooltip(bContext * /*C*/,
-                                            wmDrag *drag,
-                                            const int /*xy*/[2],
-                                            wmDropBox * /*drop*/)
+static std::string node_interface_item_drop_tooltip_single(bContext * /*C*/,
+                                                           wmDrag *drag,
+                                                           const int /*xy*/[2],
+                                                           wmDropBox * /*drop*/)
 {
   auto *drag_data = static_cast<bke::node_interface::bNodeTreeInterfaceItemReference *>(
       drag->poin);
-  const bNodeTreeInterfaceSocket *socket =
-      bke::node_interface::get_item_as<bNodeTreeInterfaceSocket>(drag_data->item);
+  bool has_selected_header_toggle = false;
 
-  if (socket) {
-    return fmt::format(fmt::runtime(TIP_("Add \"{}\" Input")), socket->name);
-  }
-  else {
-    const bNodeTreeInterfacePanel *panel =
-        bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(drag_data->item);
-    socket = panel->header_toggle_socket();
-
-    /* Dragging a panel with toggle defaults to dragging the toggle socket.
-     * Display a hint with the modifier required to drag the panel. */
-    if (socket) {
-      return fmt::format(fmt::runtime(TIP_("Add \"{}\" Input (Ctrl to add panel)")), socket->name);
+  for (int i = 0; i < drag_data->items_count; i++) {
+    const bNodeTreeInterfaceItem *item = drag_data->items[i];
+    if (const auto *panel = bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(item)) {
+      if (panel->header_toggle_socket()) {
+        has_selected_header_toggle = true;
+        break;
+      }
     }
   }
-  BLI_assert_unreachable();
-  return "Error: Unsupported socket.";
+
+  if (has_selected_header_toggle) {
+    return TIP_("Add Group Input (hold Ctrl to include sockets under panel toggle)");
+  }
+  return TIP_("Add Group Input");
 }
 
-static std::string node_panel_drop_tooltip(bContext * /*C*/,
-                                           wmDrag *drag,
-                                           const int /*xy*/[2],
-                                           wmDropBox * /*drop*/)
+static std::string node_interface_item_drop_tooltip_all(bContext * /*C*/,
+                                                        wmDrag * /*drag*/,
+                                                        const int /*xy*/[2],
+                                                        wmDropBox * /*drop*/)
 {
-  auto *drag_data = static_cast<bke::node_interface::bNodeTreeInterfaceItemReference *>(
-      drag->poin);
-  const bNodeTreeInterfacePanel *panel = bke::node_interface::get_item_as<bNodeTreeInterfacePanel>(
-      drag_data->item);
-  BLI_assert(panel);
-  return fmt::format(fmt::runtime(TIP_("Add \"{}\" Panel")), panel->name);
+  return TIP_("Add Group Input");
 }
 
 /* this region dropbox definition */
@@ -1307,16 +1231,16 @@ static void node_dropboxes()
                  nullptr);
   WM_dropbox_add(lb,
                  "NODE_OT_add_group_input_node",
-                 node_socket_drop_poll,
-                 node_socket_drop_copy,
+                 node_interface_item_drop_poll_single,
+                 node_interface_item_drop_copy_single,
                  nullptr,
-                 node_socket_drop_tooltip);
+                 node_interface_item_drop_tooltip_single);
   WM_dropbox_add(lb,
                  "NODE_OT_add_group_input_node",
-                 node_panel_drop_poll,
-                 node_panel_drop_copy,
+                 node_interface_item_drop_poll_all,
+                 node_interface_item_drop_copy_all,
                  nullptr,
-                 node_panel_drop_tooltip);
+                 node_interface_item_drop_tooltip_all);
 }
 
 /* ************* end drop *********** */

commit 3514a2d0a40ef8322003f34a83b327e9306e8c0f

Cleanup: Nodes: Quiet unused variable warning
Caused by https://projects.blender.org/blender/blender/commit/e70ba644f15a4c5f702934615e6496e7698c1892

Pull Request: https://projects.blender.org/blender/blender/pulls/157964

source/blender/editors/interface/templates/interface_template_node_tree_interface.cc
@@ -47,13 +47,10 @@ class NodeTreeInterfaceView;
 
 class NodeTreeInterfaceDragController : public AbstractViewItemDragController {
  private:
-  bNodeTreeInterfaceItem &item_;
   bNodeTree &tree_;
 
  public:
-  explicit NodeTreeInterfaceDragController(NodeTreeInterfaceView &view,
-                                           bNodeTreeInterfaceItem &item,
-                                           bNodeTree &tree);
+  explicit NodeTreeInterfaceDragController(NodeTreeInterfaceView &view, bNodeTree &tree);
   ~NodeTreeInterfaceDragController() override = default;
 
   std::optional<eWM_DragDataType> get_drag_type() const override;
@@ -366,7 +363,7 @@ std::unique_ptr<AbstractViewItemDragController> NodeSocketViewItem::create_drag_
     return nullptr;
   }
   return std::make_unique<NodeTreeInterfaceDragController>(
-      static_cast<NodeTreeInterfaceView &>(this->get_tree_view()), socket_.item, nodetree_);
+      static_cast<NodeTreeInterfaceView &>(this->get_tree_view()), nodetree_);
 }
 
 std::unique_ptr<TreeViewItemDropTarget> NodeSocketViewItem::create_drop_target()
@@ -380,7 +377,7 @@ std::unique_ptr<AbstractViewItemDragController> NodePanelViewItem::create_drag_c
     return nullptr;
   }
   return std::make_unique<NodeTreeInterfaceDragController>(
-      static_cast<NodeTreeInterfaceView &>(this->get_tree_view()), panel_.item, nodetree_);
+      static_cast<NodeTreeInterfaceView &>(this->get_tree_view()), nodetree_);
 }
 
 std::unique_ptr<TreeViewItemDropTarget> NodePanelViewItem::create_drop_target()
@@ -389,9 +386,8 @@ std::unique_ptr<TreeViewItemDropTarget> NodePanelViewItem::create_drop_target()
 }
 
 NodeTreeInterfaceDragController::NodeTreeInterfaceDragController(NodeTreeInterfaceView &view,
-                                                                 bNodeTreeInterfaceItem &item,
                                                                  bNodeTree &tree)
-    : AbstractViewItemDragController(view), item_(item), tree_(tree)
+    : AbstractViewItemDragController(view), tree_(tree)
 {
 }
 